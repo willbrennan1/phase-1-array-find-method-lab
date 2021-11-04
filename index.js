5f069705da8334fe2fdbbe2b5570ce7bfa7c5f52
Will Brennan <willbrennan314@gmail.com>
2021-11-04 11:14:00 -0400

    commit tree 6c0d4a320090d78129f181d71845bee2154ce8ad

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function superbowlWin(array) {
+    for (const record of array) {
+        if(record.result === "W") {
+            return record.year;
+        } 
+    }
+}
 
+console.log(superbowlWin(record));
